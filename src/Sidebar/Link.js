e37280e323d74060af50b47811309a5bbbbcba11
Luciano <luchosr@gmail.com>
2019-05-29 14:32:31 -0300

    add icons styling

--- src/Sidebar/Link.js
+++ src/Sidebar/Link.js
@@ -1,16 +1,31 @@
 import React from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faWrench,
+  faCode,
+  faChartLine,
+  faPencilAlt,
+  faLightbulb,
+  faQuestion
+} from "@fortawesome/free-solid-svg-icons";
 import { faStackOverflow } from "@fortawesome/free-brands-svg-icons";
-import { faWrench, faCode } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faWrench, faCode);
+library.add(
+  faWrench,
+  faCode,
+  faStackOverflow,
+  faChartLine,
+  faPencilAlt,
+  faLightbulb,
+  faQuestion
+);
 
 const Link = props => {
   return (
     <div className="sidebar__link">
       <FontAwesomeIcon
-        icon="wrench"
+        icon={props.icon}
         style={{ backgroundColor: props.color }}
         className="sidebar__link__icon"
       />
--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -7,6 +7,15 @@ const Sidebar = () => {
     <div className="sidebar">
       <div className="sidebar__links__section">
         <Link color="#8be9fd" text="settings" icon="wrench" />
+        <Link color="#50FA7B" text="gists" icon="code" />
+        <Link color="#FFB86C" text="stack overflow" icon="faStackOverflow" />
+        <Link color="#FF79C5" text="trending" icon="chart-line" />
+        <Link color="#BD93F9" text="scratch pad" icon="pencil-alt" />
+        <Link color="#F8F8F2" text="playground" icon="lightbulb" />
+        <Link color="#E6DA74" text="help/intro" icon="question" />
+      </div>
+      <div className="sidebar__logo">
+        <div className="logoImage" />
       </div>
     </div>
   );
