726addc1b25d88f265e5b9c0f0d6b91c00da5e66
Luciano <luchosr@gmail.com>
2019-05-30 20:03:09 -0300

    add jsreddit overflow styling

--- src/RedditSection/RedditItem.js
+++ src/RedditSection/RedditItem.js
@@ -5,7 +5,7 @@ const RedditItem = props => (
   <li className="jsreddit__item">
     <h5 className="jsreddit__item__title">{props.title}</h5>
     <h4 className="jsreddit__item__author" style={{ color: "#66d9ef" }}>
-      Posted by: <span style={{ color: "#f1fa8c" }}>{props.author}</span>{" "}
+      Posted by: <span style={{ color: "#f1fa8c" }}>{props.author}</span>
     </h4>
     <span style={{ color: "#6272a4" }}>
       Reddit Score: <span>{props.score} </span>
