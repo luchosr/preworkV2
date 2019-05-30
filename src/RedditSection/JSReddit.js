0f546ba4afde723498dd328e8ac838d05c0cedc0
Luciano <luchosr@gmail.com>
2019-05-30 11:32:53 -0300

    add JSreddit component

---
+++ src/RedditSection/JSReddit.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const JSReddit = () => (
+  <div>
+    <h4 className="title">popular on r/javascript</h4>
+    <ul className="jsreddit__ulist" />
+  </div>
+);
+
+export default JSReddit;
