a8b52088496180bf4b39c4bbf2af4e117e65682c
Luciano <luchosr@gmail.com>
2019-05-30 19:36:56 -0300

    add jsreddit list item styles

--- src/RedditSection/RedditItem.js
+++ src/RedditSection/RedditItem.js
@@ -4,12 +4,18 @@ import "./styles.css";
 const RedditItem = props => (
   <li className="jsreddit__item">
     <h5 className="jsreddit__item__title">{props.title}</h5>
-    <h4 className="jsreddit__item__author"> Posted by: {props.author}</h4>
-
-    <span>
+    <h4 className="jsreddit__item__author" style={{ color: "#66d9ef" }}>
+      Posted by: <span style={{ color: "#f1fa8c" }}>{props.author}</span>{" "}
+    </h4>
+    <span style={{ color: "#6272a4" }}>
       Reddit Score: <span>{props.score} </span>
-    </span>
-    <a href={props.url} className="jsreddit__item__link">
+    </span>{" "}
+    <br />
+    <a
+      href={props.url}
+      className="jsreddit__item__link"
+      style={{ color: "#ff79c6" }}
+    >
       Link to Comments
     </a>
   </li>
