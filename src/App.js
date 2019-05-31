3f834c9c4d8f26ce5991e49f84f70a55812c3d20
Luciano <luchosr@gmail.com>
2019-05-31 12:43:54 -0300

    add jshistory styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Sidebar from "./Sidebar/Sidebar";
 import JSReddit from "./RedditSection/JSReddit";
+import JShistory from "./JShistory/JShistory";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
       <JSReddit />
+      <JShistory />
     </div>
   );
 }
--- src/JShistory/JShistory.js
+++ src/JShistory/JShistory.js
@@ -1,8 +1,18 @@
 import React from "react";
+import "./styles.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
+library.add(faClock);
 const JShistory = () => (
-  <div>
-    <div className="jslinks">aasd</div>
+  <div className="jshistory">
+    <div className="jshistory__links">
+      <h4 className="jshistory__title">
+        <FontAwesomeIcon icon="clock" style={{ marginRight: "6px" }} />
+        Relevant javascript history
+      </h4>
+    </div>
   </div>
 );
 
