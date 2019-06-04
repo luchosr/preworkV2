b193c7ff62325ad525e77dc024dc6bbadfbe98a7
Luciano <luchosr@gmail.com>
2019-06-04 18:17:59 -0300

    add github issues styles

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import Sidebar from "./Sidebar/Sidebar";
 import JSReddit from "./RedditSection/JSReddit";
 import JShistory from "./JShistory/JShistory";
-
+import GithubIssues from "./GithubIssues/GithubIssues";
 function App() {
   return (
     <div className="App">
       <Sidebar />
       <JSReddit />
       <JShistory />
+      <GithubIssues />
     </div>
   );
 }
--- src/GithubIssues/GithubIssues.js
+++ src/GithubIssues/GithubIssues.js
@@ -1,13 +1,37 @@
 import React from "react";
+import "./styles.css";
+import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faGithubAlt);
 
 const GithubIssues = () => (
-  <div className="main">
-    div.
-    <h4 className="title">Github Issues History</h4>
-    <a href="https://github.com" className="subtitle">
+  <div className="githubIssues">
+    <div className="issuesSection">
       {" "}
-      switch to gitlab
-    </a>
+      <h4 className="title">
+        {" "}
+        <FontAwesomeIcon icon={faGithubAlt} /> Github Issues History
+      </h4>
+      <a href="https://github.com" className="subtitle">
+        {" "}
+        switch to gitlab
+      </a>
+      <div className="changeSub">
+        Issues <span>Pull requests</span>
+      </div>
+      <ul className="issuesList">
+        <li className="issues">
+          <a href="" className="repoLink">
+            <h5 className="repo">Repo: luchosr/Product-Landing-Page</h5>
+          </a>
+          <h6 className="issueNumber">Issue #2</h6>
+          <p className="issueTitle">Make fakeForEach method page</p>
+        </li>
+      </ul>
+    </div>
   </div>
 );
 export default GithubIssues;
