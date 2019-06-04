b897cb1979c62480c375eb3b36156371a6f3abe1
Luciano <luchosr@gmail.com>
2019-06-04 15:24:23 -0300

    add issues data

---
+++ src/GithubIssues/GithubIssues.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const GithubIssues = () => (
+  <div className="main">
+    div.
+    <h4 className="title">Github Issues History</h4>
+    <a href="https://github.com" className="subtitle">
+      {" "}
+      switch to gitlab
+    </a>
+  </div>
+);
+export default GithubIssues;
---
+++ src/GithubIssues/IssuesData.js
@@ -0,0 +1,59 @@
+const IssuesData = [
+  {
+    repo: "luchosr/Product-Landing-Page",
+    issueNumber: 348,
+    issue: "Submit error: Not in the workspace"
+  },
+  {
+    repo: "luchosr/Product-Landing-Page",
+    issueNumber: 348,
+    issue: "Submit error: Not in the workspace"
+  },
+  {
+    repo: "luchosr/Product-Landing-Page",
+    issueNumber: 348,
+    issue: "Submit error: Not in the workspace"
+  },
+  {
+    repo: "luchosr/Product-Landing-Page",
+    issueNumber: 348,
+    issue: "Submit error: Not in the workspace"
+  },
+  {
+    repo: "luchosr/Product-Landing-Page",
+    issueNumber: 348,
+    issue: "Submit error: Not in the workspace"
+  },
+  {
+    repo: "luchosr/Product-Landing-Page",
+    issueNumber: 348,
+    issue: "Submit error: Not in the workspace"
+  },
+  {
+    repo: "luchosr/Product-Landing-Page",
+    issueNumber: 348,
+    issue: "Submit error: Not in the workspace"
+  },
+  {
+    repo: "luchosr/Product-Landing-Page",
+    issueNumber: 348,
+    issue: "Submit error: Not in the workspace"
+  },
+  {
+    repo: "luchosr/Product-Landing-Page",
+    issueNumber: 348,
+    issue: "Submit error: Not in the workspace"
+  },
+  {
+    repo: "luchosr/Product-Landing-Page",
+    issueNumber: 348,
+    issue: "Submit error: Not in the workspace"
+  },
+  {
+    repo: "luchosr/Product-Landing-Page",
+    issueNumber: 348,
+    issue: "Submit error: Not in the workspace"
+  }
+];
+
+export default IssuesData;
