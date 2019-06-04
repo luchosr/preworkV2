a55fe40546f6b1737b7ffef8b08d28f4b5b29b12
Luciano <luchosr@gmail.com>
2019-06-04 11:06:27 -0300

    fix clock styles

--- src/JShistory/Clock.js
+++ src/JShistory/Clock.js
@@ -1,4 +1,12 @@
 import React, { Component } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faQuestionCircle,
+  faTimesCircle
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(faQuestionCircle, faTimesCircle);
 
 class Clock extends Component {
   state = {
@@ -27,13 +35,22 @@ class Clock extends Component {
     return (
       <div className="timer">
         <h2 className="timer__hour">
-          4:26 <span className="timer__hour__zone">PM</span>{" "}
+          4:26<span className="timer__hour__zone">PM</span>{" "}
         </h2>
-        <h5 className="timer__date">Jun 4, 2019</h5>
+        <h5 className="timer__date">June 4, 2019</h5>
         <p className="timer__messaje">
+          <span>
+            <FontAwesomeIcon icon="question-circle" />
+          </span>
           Did you know you can add CUSTOM LINKS to display in this section? Open
           the SETTINGS panel and add the URLs of your favorites sites to the
           'CUSTOM LINKS' textarea.
+          <div className="timer__messaje__hider">
+            <span>
+              <FontAwesomeIcon icon="times-circle" />
+            </span>
+            Got it! Don't show me this again
+          </div>
         </p>
       </div>
     );
