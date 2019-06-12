38c5a361f840cb16124376461fceae84da1bb0a9
Luciano <luchosr@gmail.com>
2019-06-12 10:38:57 -0300

    added useeffect hook to jsreddit component

--- src/RedditSection/JSReddit.js
+++ src/RedditSection/JSReddit.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import RedditItem from "./RedditItem";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -7,54 +7,49 @@ import { faRedditAlien } from "@fortawesome/free-brands-svg-icons";
 
 library.add(faRedditAlien);
 
-const JSReddit =() => {
+const JSReddit = () => {
+  const [error, seterror] = useState(null);
+  const [isLoading, setisLoading] = useState(false);
+  const [redditPost, setredditPost] = useState([]);
 
-const [error, seterror] = useState(null);
-const [isLoading, setisLoading] = useState(false);
-const [redditPost, setredditPost] = useState([]);
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-
-  componentDidMount() {
-    this.fetchRedditPosts();
-  }
-
-  fetchRedditPosts = async () => {
-    this.setState({ isLoading: true });
+  const fetchItems = async () => {
+    setisLoading(true);
     await fetch("https://www.reddit.com/r/javascript.json")
       .then(response => response.json())
       .then(response => {
         const { children } = response.data;
-        this.setState({
-          redditPost: children
-        });
+        setredditPost(children);
       })
-      .catch(error => this.setState({ error }))
-      .finally(() => this.setState({ isLoading: false }));
+      .catch(error => seterror(error))
+      .finally(() => setisLoading(false));
   };
-  render() {
-    const { error, isLoading, redditPost } = this.state;
-    return (
-      <div className="jsreddit">
-        <h4 className="jsreddit__title">
-          <FontAwesomeIcon icon={faRedditAlien} /> popular on r/javascript
-        </h4>
-        <ul className="jsreddit__ulist">
-          {redditPost.length > 0 &&
-            redditPost.map(post => (
-              <RedditItem
-                key={post.data.id}
-                title={post.data.title}
-                author={post.data.author}
-                url={post.data.url}
-                score={post.data.score}
-              />
-            ))}
-          {error && <h2 style={{ color: "red" }}>{error}</h2>}
-          {isLoading && <h2 style={{ color: "blue" }}>loading ....</h2>}
-        </ul>
-      </div>
-    );
-  }
-}
+
+  return (
+    <div className="jsreddit">
+      <h4 className="jsreddit__title">
+        <FontAwesomeIcon icon={faRedditAlien} /> popular on r/javascript
+      </h4>
+      <ul className="jsreddit__ulist">
+        {!error &&
+          redditPost.length > 0 &&
+          redditPost.map(post => (
+            <RedditItem
+              key={post.data.id}
+              title={post.data.title}
+              author={post.data.author}
+              url={post.data.url}
+              score={post.data.score}
+            />
+          ))}
+        {error && <h2 style={{ color: "red" }}>{error}</h2>}
+        {isLoading && <h2 style={{ color: "blue" }}>loading ....</h2>}
+      </ul>
+    </div>
+  );
+};
 
 export default JSReddit;
