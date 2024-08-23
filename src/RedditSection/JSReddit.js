c30d5e4641adf123baf5afe10ebc3a57bbbc35c2
Luciano Ramello <luchosr@gmail.com>
2024-08-23 14:02:52 +0200

    feat: updates fetch items methods

--- src/RedditSection/JSReddit.js
+++ src/RedditSection/JSReddit.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import "./styles.css";
-import RedditItem from "./RedditItem";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRedditAlien } from "@fortawesome/free-brands-svg-icons";
+import React, { useState, useEffect } from 'react';
+import './styles.css';
+import RedditItem from './RedditItem';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRedditAlien } from '@fortawesome/free-brands-svg-icons';
 
 library.add(faRedditAlien);
 
@@ -17,26 +17,28 @@ const JSReddit = () => {
   }, []);
 
   const fetchItems = async () => {
-    setisLoading(true);
-    await fetch("https://www.reddit.com/r/javascript.json")
-      .then(response => response.json())
-      .then(response => {
-        const { children } = response.data;
-        setredditPost(children);
-      })
-      .catch(error => seterror(error))
-      .finally(() => setisLoading(false));
+    try {
+      setisLoading(true);
+      const response = await fetch('https://www.reddit.com/r/javascript.json');
+      const { data } = await response.json();
+      const { children } = data;
+      setredditPost(children);
+    } catch (error) {
+      seterror(error);
+    } finally {
+      setisLoading(false);
+    }
   };
 
   return (
-    <div className="jsreddit">
+    <section className="jsreddit">
       <h4 className="jsreddit__title">
         <FontAwesomeIcon icon={faRedditAlien} /> popular on r/javascript
       </h4>
       <ul className="jsreddit__ulist">
         {!error &&
-          redditPost.length > 0 &&
-          redditPost.map(post => (
+          redditPost?.length > 0 &&
+          redditPost.map((post) => (
             <RedditItem
               key={post.data.id}
               title={post.data.title}
@@ -45,10 +47,10 @@ const JSReddit = () => {
               score={post.data.score}
             />
           ))}
-        {error && <h2 style={{ color: "red" }}>{error}</h2>}
-        {isLoading && <h2 style={{ color: "blue" }}>loading ....</h2>}
+        {error && <h2 style={{ color: 'red' }}>{error}</h2>}
+        {isLoading && <h2 style={{ color: 'blue' }}>loading ....</h2>}
       </ul>
-    </div>
+    </section>
   );
 };
 
